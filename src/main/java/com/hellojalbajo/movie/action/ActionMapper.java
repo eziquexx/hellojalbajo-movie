4b3862a3d1fcead5a0f23cd2b2909b70a9678061
Oonet96 <captaing96@gmail.com>
2024-10-10 11:22:21 +0900

    24.10.10 HT :  ActionMapper 작업 완료

---
+++ src/main/java/com/hellojalbajo/movie/action/ActionMapper.java
@@ -0,0 +1,16 @@
+package com.hellojalbajo.movie.action;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.hellojalbajo.movie.Movie;
+
+
+@Mapper
+public interface ActionMapper {
+
+    public List<Movie> getMovieList();
+    public Movie getMoviebyId(int id);
+    public void setActionMovie(Movie movie);
+}
