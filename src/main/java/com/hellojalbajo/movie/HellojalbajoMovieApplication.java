5823f619a9d2521e8af11a28c8a4bf2bd9dd092e
jelee <waftyann@gmail.com>
2024-10-10 09:49:30 +0900

    프로젝트 생성 및 초기화

---
+++ src/main/java/com/hellojalbajo/movie/HellojalbajoMovieApplication.java
@@ -0,0 +1,13 @@
+package com.hellojalbajo.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellojalbajoMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellojalbajoMovieApplication.class, args);
+	}
+
+}
