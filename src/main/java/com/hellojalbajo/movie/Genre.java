cdd0f1c896e26bf6fb53cae68b3933aedb33a826
ssssRssssungjune <ssungjune0610@gmail.com>
2024-10-10 12:46:55 +0900

    12시46분 수정

--- src/main/java/com/hellojalbajo/movie/Genre.java
+++ src/main/java/com/hellojalbajo/movie/Genre.java
@@ -1,12 +1,10 @@
 package com.hellojalbajo.movie;
 
 public class Genre {
-
     private Integer id;
     private String moviename;
-    private Integer productionYear;
+    private Integer production_year; // 필드 이름 확인
     private String description;
-
 
     public Integer getId() {
         return id;
@@ -16,7 +14,6 @@ public class Genre {
         this.id = id;
     }
 
-
     public String getMoviename() {
         return moviename;
     }
@@ -25,15 +22,13 @@ public class Genre {
         this.moviename = moviename;
     }
 
-
-    public Integer getProduction_year() {
-        return productionYear;
+    public Integer getProductionYear() {
+        return production_year;
     }
 
-    public void setProduction_year(Integer productionYear) {
-        this.productionYear = productionYear;
+    public void setProductionYear(Integer productionYear) {
+        this.production_year = production_year;
     }
-
 
     public String getDescription() {
         return description;
@@ -43,7 +38,3 @@ public class Genre {
         this.description = description;
     }
 }
-
-
-
-
