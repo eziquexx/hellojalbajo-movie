5d1cc72205ffec919d7ce9605f1e8ca4eef98c44
Oonet96 <captaing96@gmail.com>
2024-10-10 16:52:36 +0900

    24.10.10 HT : MovieList.html 생성 및 연결

--- src/main/java/com/hellojalbajo/movie/action/ActionController.java
+++ src/main/java/com/hellojalbajo/movie/action/ActionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,7 @@ public class ActionController {
     @Autowired
     private ActionService _service;
 
-    @GetMapping
+    @GetMapping("list")
     @ResponseBody
     public List<Movie> GETActionMovieList(){
 
@@ -34,9 +35,16 @@ public class ActionController {
         return _service.getActionMovie(id);
     }
 
-    @PostMapping
+    @PostMapping("create")
     public void POSTSetActionMovie(@RequestBody Movie movie){
         _service.setActionMovie(movie);
+    }
+
+    @GetMapping
+    public String GETMovieListPage(Model model) {
+        model.addAttribute("title","action");
+        model.addAttribute("lstData",_service.getActionMovieList());
+        return "MovieList";
     }
 
 }
